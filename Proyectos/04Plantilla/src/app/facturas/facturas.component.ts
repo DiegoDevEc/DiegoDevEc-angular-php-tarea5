61f42e4b31b7d048acc6292395078a0f6c9241f0
Diego Acosta <diego.acosta@externos.bci.cl>
2024-09-01 23:20:46 -0500

    Commit inicial

---
+++ Proyectos/04Plantilla/src/app/facturas/facturas.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { SharedModule } from 'src/app/theme/shared/shared.module';
+import { IFactura } from '../Interfaces/factura';
+import { Router, RouterLink } from '@angular/router';
+import { FacturaService } from '../Services/factura.service';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-facturas',
+  standalone: true,
+  imports: [SharedModule, RouterLink],
+  templateUrl: './facturas.component.html',
+  styleUrl: './facturas.component.scss'
+})
+export class FacturasComponent implements OnInit {
+  listafacturas: IFactura[] = [];
+  constructor(private facturaServicio: FacturaService) {}
+  ngOnInit(): void {
+    this.cargatabla();
+  }
+  cargatabla() {
+    this.facturaServicio.todos().subscribe((data: IFactura[]) => {
+      this.listafacturas = data;
+    });
+  }
+
+  eliminar(idFactura) {
+    Swal.fire({
+      title: 'Facturas',
+      text: 'Esta seguro que desea eliminar la factura!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#d33',
+      cancelButtonColor: '#3085d6',
+      confirmButtonText: 'Eliminar factura'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.facturaServicio.eliminar(idFactura).subscribe((data) => {
+          Swal.fire('Facturas', 'La factura ha sido eliminada.', 'success');
+          this.cargatabla();
+        });
+      }
+    });
+  }
+}
